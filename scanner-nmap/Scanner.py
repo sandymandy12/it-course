473e43fbc3e0a1b7174fd453f7dfa6c0fc4c1633
Devin Catuns <deecat10@gmail.com>
2022-01-18 15:12:38 -0500

    update

---
+++ scanner-nmap/Scanner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print('Welcome to Scanner')
+print('<---------------------------------------------------------------->')
+
+ip_addr = input("Enter ip address to scan\n")
+
+print("Correct IP?: " + ip_addr)
+
+scanner.scan(ip_addr, '1-1024', '-V -sS')
+print(scanner.scaninfo())
+print('IP status', scanner[ip_addr].state())
+print(scanner[ip_addr].all_protocols())
+print('Open ports: ', scanner[ip_addr]['tcp'].keys())
